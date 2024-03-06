4ae31196ead07f9a2962db3d5562e440947ea86f
MilenaRickliS <141282223+MilenaRickliS@users.noreply.github.com>
2024-03-05 22:01:00 -0300

    commit

--- My project/Assets/Manager/manager.cs
+++ My project/Assets/Manager/manager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class manager : MonoBehaviour
 {
-    private void Update()
+    void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition)
-            var obj = Physics2D.OverlapPoint(mousePosition)
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var obj = Physics2D.OverlapPoint(mousePosition);
             if(obj != null){
                 if(obj.gameObject.TryGetCoomponent<Itouch>(out var touch)){
                     touch.Touch();
